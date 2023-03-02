fa7db0f5f04b85c7ac068bfb287151f52219b23a
Javier-Santamaria <126354748+Javier-Santamaria@users.noreply.github.com>
2023-03-02 16:57:41 -0500

    Add files via upload

---
+++ Practica 01/practica01.js
@@ -0,0 +1,10 @@
+const inputRandomNumber = document.getElementById('inputRandom');
+
+window.addEventListener('load', () => {
+    getRandomNumber();
+})
+
+function getRandomNumber() {
+    let randomNumber = Math.random().toString().slice(2, 8);
+    inputRandomNumber.value = randomNumber;
+}
